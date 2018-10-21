1ae2e6805a63b639fe617844b5da7996564e6793
Benjamin Caure <bcaure@gmail.com>
2018-10-21 15:08:19 +0200

    :bug: form validation

--- front/src/Login.js
+++ front/src/Login.js
@@ -4,6 +4,8 @@ import './Login.css';
 export default class Login extends Component {
     constructor(props) {
       super(props);
+      this.usernameInput = React.createRef();
+      this.passwordInput = React.createRef();
       this.state = {
         username: '',
         password: ''
@@ -12,16 +14,26 @@ export default class Login extends Component {
     handleChange(e) {
       this.setState({[e.target.name]: e.target.value});
     }
+
+    validateAndSubmit(e) {
+      if (Object.values(this.refs).reduce((prev, curr) => prev && curr.checkValidity())) {
+        this.props.onSubmit(this.state.username, this.state.password);
+      }
+    }
   
     render() {
       return (
         <form className="wrap flex-center">
           <div>
-            <input type="text" required placeholder="username" name="username" value={this.state.username} onChange={(e) => this.handleChange(e)} />
-            <input type="password" required placeholder="password" name="password" value={this.state.password} onChange={(e) => this.handleChange(e)} />
+            <input type="text" required placeholder="username" name="username"
+              ref="usernameInput"
+              value={this.state.username} onChange={(e) => this.handleChange(e)} />
+            <input type="password" required placeholder="password" name="password" 
+              ref="passwordInput"
+              value={this.state.password} onChange={(e) => this.handleChange(e)} />
           </div>
           <div>
-            <button className="round flash" onClick={() => this.props.onSubmit(this.state.username, this.state.password)}><i className="material-icons">done</i></button>
+            <button className="round flash" onClick={(e) => this.validateAndSubmit(e)}><i className="material-icons">done</i></button>
           </div>
         </form>
       );
