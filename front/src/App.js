d5a362ef119c40a7541a061281c62bcaf755810d
Benjamin Caure <bcaure@gmail.com>
2019-03-28 11:14:27 +0100

    :bug: fix logo

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo1.png';
+import logo from './logo.png';
 import './App.css';
 import Login from './Login';
 import Table from './Table';
